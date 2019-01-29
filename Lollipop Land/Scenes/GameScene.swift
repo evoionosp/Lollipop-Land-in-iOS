d66614fc69499b6ebb530eaa0f6e62fd733fab43
Shubh Patel <patelshubh96@gmail.com>
2019-01-29 00:55:22 -0500

    Started developing

---
+++ Lollipop Land/Scenes/GameScene.swift
@@ -0,0 +1,32 @@
+//
+//  GameScene.swift
+//  Lollipop Land
+//
+//  Created by Shubh Patel on 2019-01-29.
+//  Copyright © 2019 Shubh Patel. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    
+    override func sceneDidLoad() {
+
+    }
+    
+    override func didMove(to view: SKView) {
+        
+    }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+       
+    }
+    
+    
+    override func update(_ currentTime: TimeInterval) {
+
+    }
+}
---
+++ Lollipop Land/ViewControllers/GameViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GameViewController.swift
+//  Lollipop Land
+//
+//  Created by Shubh Patel on 2019-01-29.
+//  Copyright © 2019 Shubh Patel. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        // Present the scene
+        if let view = self.view as! SKView? {
+            let scene = GameScene(size: view.bounds.size)
+            scene.scaleMode = .aspectFill
+            
+            view.presentScene(scene)
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
